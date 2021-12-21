557646a928eca7e72832c5d8c144574df2b51fa1
Evyatar Haim <evyataros1@gmail.com>
2021-12-21 09:00:40 +0200

    improve buttons style

--- src/components/subComponents/Buttons/CustomButton.js
+++ src/components/subComponents/Buttons/CustomButton.js
@@ -2,17 +2,39 @@ import React from 'react';
 import { Button, makeStyles } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
+    btn: {
+        fontFamily: 'Poppins !important',
+        fontsize: "12 !important",
+    },
+    primarybtn: {
+        backgroundColor: "#2E3F50",
+        color: "#FFFFFF",
+        '&:hover': {
+            backgroundColor: "#2E3F70",
+        },
+    },
+    secondarybtn: {
+        border: "1px solid #2E3F50",
+        color: "#2E3F50",
+        marginRight: theme.spacing(1),
+    },
+
 
 }));
 
 
 const CustomButton = ({ title, onClick, type }) => {
-    // const classes = useStyles();
+    const classes = useStyles();
 
     return (
         <Button
-            // classes={{ color: classes.color }} 
-            onClick={onClick} color="primary" type={type}>{title}</Button>
+            className={[classes.btn, type ? classes.primarybtn : classes.secondarybtn].join(" ")}
+            onClick={onClick}
+            variant="outlined"
+            size="medium"
+            type={type}>
+            {title}
+        </Button>
     )
 }
 
